2e886e443b9ba85408098f0b173a9d58c72ec5c4
nurhanamkkk <nurhanamk.30@gmail.com>
2024-05-11 10:05:50 +0700

    menambah titik koma pada akhir kurung kurawal

--- StackC++.cpp
+++ StackC++.cpp
@@ -4,5 +4,11 @@ using namespace std;
 
 // Node class representing a single node in the linked list
 class Node {
-    
-}
+public:
+     int data;
+     Node* next;
+
+     Node() {
+         next = NULL;
+     }
+};
