5df3f291e0f0e84b578a53bd6d7574b2433af615
nurhanamkkk <nurhanamk.30@gmail.com>
2024-05-11 10:25:34 +0700

    menambah titik koma pada akhir kurung kurawal

--- StackC++.cpp
+++ StackC++.cpp
@@ -45,6 +45,21 @@ public:
 
        // Peek/Top operation: Retrieve the value of the topmost element without removing it
        void peek() {
-        
+           if (top== NULL) {
+               cout << "List is empty." << endl;
+           }
+           else {
+              Node* current = top;
+              while (current != NULL) {
+                   cout << current->data << " " << endl;
+                   current = current->next;
+              }
+               cout << endl;
+           }// Return the value of the top node
        }
- }
+
+       // isEmpty operation: Check if the stack is empty
+       bool isEmpty() {
+             return top == NULL; // Return true if the top pointer is NULL, indicating an empty stack
+       }
+ };
