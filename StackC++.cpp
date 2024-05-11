5e14b3eefb2c5a8db265630e49d8de3c66970653
nurhanamkkk <nurhanamk.30@gmail.com>
2024-05-11 10:16:36 +0700

    membuat prosedur pop

--- StackC++.cpp
+++ StackC++.cpp
@@ -25,6 +25,16 @@ public:
 
       // Push operation: Insert an element onto the top of the  stack
       int push(int value) {
-        
+           Node* newNode = new Node(); // 1. Allocate memory for the new node
+           newNode->data = value; // 2. Assign value
+           newNode->next = top; // 3. set the next pointer of the new node to the current top node
+           top = newNode; // 4. Update the top pointer to the new node 
+           cout << "Push value: " << value << endl;
+           return value;
       }
+
+       // Pop operation: remove the topmost element from the stack
+       void pop() {
+        
+       }
  }
